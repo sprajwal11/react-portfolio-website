eff82048f01261a2c103f2fa5621f9ead467de7c
Prajwal <sharmaprajwal11@gmail.com>
2022-12-18 16:21:27 +0530

    about jsx done. css left

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import './about.css'
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About=()=> {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+          
+            <article className="about_card">
+              <FiUsers className='about_icon'/>
+              <h5>Clients</h5>
+              <small>20+ Worldwide</small>
+            </article>
+          
+            <article className="about_card">
+              <VscFolderLibrary className='about_icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+          </div>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, sed molestiae enim atque sint incidunt rem error nam placeat. Voluptate ut reprehenderit incidunt sit debitis quia soluta esse, explicabo voluptates!
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+
+      </div>
+    </section>
+    
   )
 }
 
